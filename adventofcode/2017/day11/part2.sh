343c5b14c1e714bda3c4877d86471224ea443908
Phillip Alexander <glupmjoed@fafnir.dk>
2017-12-11 11:35:17 +0100

    Add input sanitation to AoC 2017 day 11

--- adventofcode/2017/day11/part2.sh
+++ adventofcode/2017/day11/part2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sed 's/,/ /g;
-     s/ne/((x+=1)); ((y-=1)); echo $x $y;/g;
-     s/sw/((x-=1)); ((y+=1)); echo $x $y;/g;
-     s/se/((y-=1)); ((z+=1)); echo $y $z;/g;
-     s/nw/((y+=1)); ((z-=1)); echo $y $z;/g;
-      s/n/((x+=1)); ((z-=1)); echo $x $z;/g;
-      s/s/((x-=1)); ((z+=1)); echo $x $z;/g;' |
+grep -oE "[ns][ew]*" |
+	sed 's/ne/((x+=1)); ((y-=1)); echo $x $y;/g;
+         s/sw/((x-=1)); ((y+=1)); echo $x $y;/g;
+         s/se/((y-=1)); ((z+=1)); echo $y $z;/g;
+         s/nw/((y+=1)); ((z-=1)); echo $y $z;/g;
+         s/n/((x+=1)); ((z-=1)); echo $x $z;/g;
+         s/s/((x-=1)); ((z+=1)); echo $x $z;/g;' |
+
 	bash | grep -oE "[0-9]+" | sort -nr | head -1
