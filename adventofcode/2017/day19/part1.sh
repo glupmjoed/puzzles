db3a069f7e7a5b735083a426016184af4f50cb6a
Phillip Alexander <glupmjoed@fafnir.dk>
2017-12-19 08:53:32 +0100

    Use compiled Go binary

--- adventofcode/2017/day19/part1.sh
+++ adventofcode/2017/day19/part1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go run walk.go | sed -r 's/[^A-Z]*([A-Z])+[^A-Z]*/\1/g' && echo
+./bin/walk | sed -r 's/[^A-Z]*([A-Z])+[^A-Z]*/\1/g' && echo
--- adventofcode/2017/day19/part2.sh
+++ adventofcode/2017/day19/part2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go run walk.go | wc -c
+./bin/walk | wc -c
