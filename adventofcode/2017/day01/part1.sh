a6484a63c01ceb68bc33015b4e7f2ae85855a70f
Phillip Alexander <glupmjoed@fafnir.dk>
2017-12-29 16:23:39 +0100

    Prune some commands by reusing awk invocation

--- adventofcode/2017/day01/part1.sh
+++ adventofcode/2017/day01/part1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 grep -o . | sed '1p; 1{1h;1!H;d}; $G' | uniq -c |
-	awk '{ printf "(%d-1)*%d+", $1, $2 }' | echo $(cat)0 | bc
+	awk '{ printf "(%d-1)*%d+", $1, $2 } END { print 0 }' | bc
