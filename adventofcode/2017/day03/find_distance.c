b59520ac146568b0ef33ccbdc5778e9f9da6cb19
Phillip Alexander <glupmjoed@fafnir.dk>
2018-01-01 17:53:23 +0100

    Use multiplication instead of bitshifting

--- adventofcode/2017/day03/find_distance.c
+++ adventofcode/2017/day03/find_distance.c
@@ -4,7 +4,7 @@
 int main() {
   int corner, square, ring = 0;
   scanf("%d", &square);
-  for (corner = 1; corner < square; corner += ++ring << 3);
+  for (corner = 1; corner < square; corner += ++ring * 8);
   if (ring == 0) { printf("1\n"); return 0; }
   int idx = corner - square;
   printf("%d\n", idx/ring%2 ? ring+(idx%ring) : ring*2-(idx%ring));
