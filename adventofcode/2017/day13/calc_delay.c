2018a521f3bda856e319f18e30656b210c486880
Phillip Alexander <glupmjoed@fafnir.dk>
2018-01-02 04:49:12 +0100

    Indent C files with spaces

--- adventofcode/2017/day13/calc_delay.c
+++ adventofcode/2017/day13/calc_delay.c
@@ -8,6 +8,6 @@ int main() {
   for (int i = lay2; scanf("%d: %d", a+--i, a+--i) == 2;);
   int delay = 0;
   for (int caps = 1; caps > 0; delay++)
-	for (int i = caps = 0; i < lay2; i+=2) (a[i+1]+delay)%(a[i]*2-2) ? : caps++;
+    for (int i = caps = 0; i < lay2; i+=2) (a[i+1]+delay)%(a[i]*2-2) ? : caps++;
   printf("%d\n", --delay);
 }
