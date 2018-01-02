104ba48440e153f6c7a186eee7006d7313ccfede
Phillip Alexander <glupmjoed@fafnir.dk>
2018-01-02 04:46:00 +0100

    (Re)use sed and tr instead of cat and echo

--- adventofcode/2017/day11/part1.sh
+++ adventofcode/2017/day11/part1.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cat <(grep -o "[ns][ew]*" |
+grep -o "[ns][ew]*" | tr -d '\n' |
     sed 's/ne/((x+=1)); ((y-=1));/g;  s/sw/((x-=1)); ((y+=1));/g;
          s/se/((y-=1)); ((z+=1));/g;  s/nw/((y+=1)); ((z-=1));/g;
-          s/n/((x+=1)); ((z-=1));/g;   s/s/((x-=1)); ((z+=1));/g;'
-     ) <(echo 'echo $x $y $z') |
-
+          s/n/((x+=1)); ((z-=1));/g;   s/s/((x-=1)); ((z+=1));/g;
+         s/$/ echo $x $y $z\n/g' |
     bash | grep -oE "[0-9]+" | sort -nr | head -1
