52b7548b9f47aed2d55ebb7644222982ce42d2ee
Phillip Alexander <glupmjoed@fafnir.dk>
2018-05-16 14:20:32 +0200

    Use paste + bc for arithmetic

--- adventofcode/2016/day04/part1.sh
+++ adventofcode/2016/day04/part1.sh
@@ -5,5 +5,5 @@
     do sum=$(echo $name | grep -o "[a-z]" | sort | uniq -c |
              sort -k 1,1rn -k 2,2 |
              awk 'NR <= 5 { printf "%s", $2 } END { print "" }')
-       if [ $sum == $checksum ]; then echo -n $id"+"; fi
-    done | echo $(cat) 0 | bc
+       if [ $sum == $checksum ]; then echo $id; fi
+    done | paste -sd+ | bc
