9af6d57ca112a9d02260c109aff88da20cd75814
Phillip Alexander <glupmjoed@fafnir.dk>
2018-05-16 16:52:10 +0200

    Use AWK, paste and bc for arithmetic

--- adventofcode/2017/day02/part1.sh
+++ adventofcode/2017/day02/part1.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 while read -r line
-do
-    echo $line | grep -oE '[0-9]+' | sort -nr |
-        awk 'NR==1 { printf "(%d-", $N }; END { printf "%d)+", $N }'
-done |
-    echo $(cat)0 | bc
+do echo $line | grep -oE '[0-9]+' | sort -n |
+        awk 'NR==1 { min = $1 } END { print $1-min }'
+done | paste -sd+ | bc
