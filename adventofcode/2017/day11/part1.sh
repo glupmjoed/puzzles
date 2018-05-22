7d961c754a0dc6c4b8448f002274fc2c2919b74b
Phillip Alexander <glupmjoed@fafnir.dk>
2018-05-22 14:23:49 +0200

    Use AWK to calculate coordinates

--- adventofcode/2017/day11/part1.sh
+++ adventofcode/2017/day11/part1.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-grep -o "[ns][ew]*" | tr -d '\n' |
-    sed 's/ne/((x+=1)); ((y-=1));/g;  s/sw/((x-=1)); ((y+=1));/g;
-         s/se/((y-=1)); ((z+=1));/g;  s/nw/((y+=1)); ((z-=1));/g;
-          s/n/((x+=1)); ((z-=1));/g;   s/s/((x-=1)); ((z+=1));/g;
-         s/$/ echo $x $y $z\n/g' |
-    bash | grep -oE "[0-9]+" | sort -nr | head -1
+grep -o "[ns][ew]*" | awk -f coords.awk | tail -1 | grep -oE "[0-9]+" | sort |
+    tail -1
