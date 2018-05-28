e053761485d761543a0e74440c6bdfcd59189b31
Phillip Alexander <glupmjoed@fafnir.dk>
2018-05-28 17:46:19 +0200

    Simplify AoC 2017 day 20 part 1

--- adventofcode/2017/day20/part1.sh
+++ adventofcode/2017/day20/part1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-awk '{ print NR-1 " " $3 }' | sed -E 's/[^0-9]*([0-9]+)[^0-9]*/ \1 /g' |
-    awk '{ printf "echo $((%s+%s+%s)) %s\n", $2, $3, $4, $1 }' | bash |
-    sort -n | awk 'NR<=5 { printf "%3d (%d)\n", $2, $1 }'
+sed 's/[^0-9,]//g' | awk -F ',' '{ print $7+$8+$9 " " NR-1 }' | sort -n |
+    head -5
