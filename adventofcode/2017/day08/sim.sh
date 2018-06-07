eb49792bd745c7996d5f1ed823a15b2c7c99b37e
Phillip Alexander <glupmjoed@fafnir.dk>
2018-06-07 19:02:36 +0200

    Solve AoC 2017 day 8 without eval / code generation

--- adventofcode/2017/day08/sim.sh
+++ adventofcode/2017/day08/sim.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-grep -E "^[a-z]+ (inc|dec) -*[0-9]+ if [a-z]+ [<>!=]=* -*[0-9]+$" |
-
-    sed 's/!=/ne/g;  s/==/eq/g;  s/inc //g;  s/dec /-/g;
-         s/<=/le/g;  s/>=/ge/g;  s/</lt/g;   s/>/gt/g;' |
-
-    awk '{ printf "if [ $((%s)) -%s %s ]; then ((%s += %s)); fi; ",$4,$5,$6,$1,$2 }
-         { print "echo $((" $1 ")) " $1 "; echo $((" $4 ")) " $4 }' |
-
-    bash
+sed -Ef stz.sed | awk -f sim.awk
