0fe5d986a2bc2175e1b9878a6e96ded3e7b3d323
Phillip Alexander <glupmjoed@fafnir.dk>
2018-09-05 18:02:25 +0200

    Add Human Resource Machine year 21

---
+++ hrm/21_zero_terminated_sum.asm
@@ -0,0 +1,16 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+
+    JUMP     c
+a:
+    COPYFROM 0
+b:
+    OUTBOX
+c:
+    INBOX
+    JUMPZ    b
+d:
+    COPYTO   0
+    INBOX
+    JUMPZ    a
+    ADD      0
+    JUMP     d
