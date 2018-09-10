085ea1fee613bb2e81112edd1d8c40ed7077596f
Phillip Alexander <glupmjoed@fafnir.dk>
2018-09-10 22:39:53 +0200

    Add Human Resource Machine year 39

---
+++ hrm/39_re-coordinator.asm
@@ -0,0 +1,19 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+
+    JUMP     b
+a:
+    ADD      15
+    OUTBOX      -- col# < 4, outbox col#
+    COPYFROM 10
+    OUTBOX      -- outbox row#
+b:
+    COPYFROM 14
+    COPYTO   10 -- row# -> r10
+    INBOX
+c:
+    SUB      15
+    JUMPN    a  -- break if n < 4
+    COPYTO   11
+    BUMPUP   10 -- increment row# if n >= 4
+    COPYFROM 11
+    JUMP     c
