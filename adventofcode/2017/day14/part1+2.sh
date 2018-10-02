286ea8dc943e546965846339bae1de7e977a7a09
Phillip Alexander <glupmjoed@fafnir.dk>
2018-10-02 23:58:48 +0200

    Fix whitespace bug

--- adventofcode/2017/day14/part1+2.sh
+++ adventofcode/2017/day14/part1+2.sh
@@ -6,4 +6,4 @@ do echo $key-$i | ./knot_hash.sh
 done | ./hex2bin.sh > bin
 cat bin | tr -dc '1' | echo "Part 1:" $(wc -c)
 
-cat bin | ./build_graph.py | gc -c | awk '{ print "Part 2: ", $1 }'; rm -f bin
+cat bin | ./build_graph.py | gc -c | awk '{ print "Part 2:", $1 }'; rm -f bin
