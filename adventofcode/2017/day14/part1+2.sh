33fed07d36916660e59872df56c8652de622eea8
Phillip Alexander <glupmjoed@fafnir.dk>
2018-10-02 23:57:52 +0200

    Use tr instead of grep to isolate '1's

--- adventofcode/2017/day14/part1+2.sh
+++ adventofcode/2017/day14/part1+2.sh
@@ -4,6 +4,6 @@ key=$(cat)
 for i in {0..127}
 do echo $key-$i | ./knot_hash.sh
 done | ./hex2bin.sh > bin
-grep -o "1" bin | echo "Part 1:" $(wc -l)
+cat bin | tr -dc '1' | echo "Part 1:" $(wc -c)
 
 cat bin | ./build_graph.py | gc -c | awk '{ print "Part 2: ", $1 }'; rm -f bin
