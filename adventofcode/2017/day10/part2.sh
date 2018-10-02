52d4b4362efb2ff31b196c197fce83b0aba32514
Phillip Alexander <glupmjoed@fafnir.dk>
2018-10-02 23:27:34 +0200

    Reformat for-loops

--- adventofcode/2017/day10/part2.sh
+++ adventofcode/2017/day10/part2.sh
@@ -2,5 +2,6 @@
 
 lseq=$(echo $(printf "%s" "$(cat)" | od -A n -t d1) "17 31 73 47 23")
 
-for _ in $(seq 64); do echo $lseq | grep -oE "[0-9]+"
+for _ in $(seq 64)
+do echo $lseq | grep -oE "[0-9]+"
 done | ./bin/sparse_hash | awk -f condense.awk
--- adventofcode/2017/day14/part1+2.sh
+++ adventofcode/2017/day14/part1+2.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-key=$(cat); for i in {0..127}; do echo $key-$i | ./knot_hash.sh; done |
-    awk 'BEGIN { print "ibase=16; obase=2;" } { print toupper($0) }' | bc > bin
+key=$(cat)
+for i in {0..127}
+do echo $key-$i | ./knot_hash.sh
+done | awk 'BEGIN { print "ibase=16; obase=2;" } { print toupper($0) }' |
+    bc > bin
 grep -o "1" bin | echo "Part 1:" $(wc -l)
 
 echo $(cat bin) | grep -oE "[01]+[^01]*[01]+" | sed 's/\\ //g' |
